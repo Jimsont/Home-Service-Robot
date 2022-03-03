def7a6c8e55319ab3786539f840418295487a1fc
Jimsont <jimsontseng@gmail.com>
2022-03-03 23:29:14 +0800

    commit

--- catkin_ws/src/add_markers/src/add_markers_test.cpp
+++ catkin_ws/src/add_markers/src/add_markers_test.cpp
@@ -13,58 +13,12 @@ float marker_pick_y = 3.33;
 float marker_drop_x = -0.5;
 float marker_drop_y = -0.5;
 
-// define position threshold for checking distance between robot and marker
-float dis_thresh = 0.5;
-
-// define odom callback function.
-// The function checks if robot is at pick up or drop off location
-// Then, set pick_up and drop_off flag
-void odom_cb(const nav_msgs::Odometry::ConstPtr& msg)
-{
-
-
-  // extract robot x and y position
-  float robot_x = msg->pose.pose.position.x;
-  float robot_y = msg->pose.pose.position.y; 
-  
-  // at startup, pick_up and drop_off are false.
-  // When robot moves to pick_up location, set pick_up flag to true
-  if(pick_up == false && drop_off == false)
-  {
-    // calculate distance between robot and pick_up location
-    float dis_pick = sqrt((robot_x-marker_pick_x)*(robot_x-marker_pick_x) + (robot_y-marker_pick_y)*(robot_y-marker_pick_y));
-    
-      
-    // if robot to pick_up location is smaller than threshold, set pick_up flag to true
-    if(dis_pick<=dis_thresh)
-    {
-      ROS_INFO("pick up marker");
-      pick_up = true;
-    }
-    
-  }
-  if(pick_up == true && drop_off == false)
-  {
-    // calculate distance between robot and drop_off location
-    float dis_drop = sqrt((robot_x-marker_drop_x)*(robot_x-marker_drop_x) + (robot_y-marker_drop_y)*(robot_y-marker_drop_y));
-      
-    // if robot to drop_off location is smaller than threshold, set drop_off flag to true
-    if(dis_drop<=dis_thresh)
-    {
-      ROS_INFO("drop off marker");
-      drop_off = true;
-    }
-  }
-}
-
-
 int main( int argc, char** argv )
 {
   ros::init(argc, argv, "add_markers");
   ros::NodeHandle n;
   ros::Rate r(1);
   ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
-  ros::Subscriber odom_sub = n.subscribe("/odom", 1000, odom_cb);
 
   // Set our initial shape type to be a cube
   uint32_t shape = visualization_msgs::Marker::CUBE;
@@ -119,23 +73,28 @@ int main( int argc, char** argv )
       ROS_WARN_ONCE("Please create a subscriber to the marker");
       sleep(1);
     }
-    
-    if(pick_up == true && drop_off == false)
-    {
-      marker.action = visualization_msgs::Marker::DELETE;
-      ros::Duration(5.0).sleep();
-      
-    }
-    if(pick_up == true && drop_off == true)
-    {
-      marker.pose.position.x = marker_drop_x;
-      marker.pose.position.y = marker_drop_y;
-      marker.action = visualization_msgs::Marker::ADD;
-      ros::Duration(5.0).sleep();
-    }
+    // Publish the marker at the pickup zone
     marker_pub.publish(marker);
-//     r.sleep();
-    ros::spinOnce();
+    
+    // wait for 5 seconds
+    ros::Duration(5).sleep();
+	
+    // Delete Marker
+    marker.action = visualization_msgs::Marker::DELETE;
+    marker_pub.publish(marker);
+    
+    // wait for 5 seconds
+    ros::Duration(5).sleep();
+    
+    // show marker at drop-off location
+    marker.pose.position.x = marker_drop_x;
+    marker.pose.position.y = marker_drop_y;
+    marker.action = visualization_msgs::Marker::ADD;
+    marker_pub.publish(marker);
+    ros::Duration(5).sleep();
+    
+    r.sleep();
+//     ros::spinOnce();
   }
   return 0;
 }
--- catkin_ws/src/scripts/add_marker.sh
+++ catkin_ws/src/scripts/add_marker.sh
@@ -9,4 +9,4 @@ xterm -e "cd ${path_catkin_ws} && source devel/setup.bash; roslaunch turtlebot_g
 sleep 5
 xterm -e "cd ${path_catkin_ws} && source devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
-xterm -e "cd ${path_catkin_ws} && source devel/setup.bash; rosrun add_markers add_markers"
+xterm -e "cd ${path_catkin_ws} && source devel/setup.bash; rosrun add_markers add_markers_test"
